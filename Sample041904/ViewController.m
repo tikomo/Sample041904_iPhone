2547e6e85772c9117f9f09be73d469e7e15918c9
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-04-19 19:38:34 +0900

    1st commit

--- Sample041904/ViewController.m
+++ Sample041904/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView1;
 
 @end
 
@@ -18,6 +19,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // case1)
+    [_tableView1 setDelegate:self];　// テーブルに表示するデータは誰に聞くの？
+    [_tableView1 setDataSource:self]; // テーブルが選択された時は誰に言えばいいの？
+    
+    // case2)
+    // _tableView1.delegate = self;
+    // _tableView1.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +34,45 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+//
+// セルの内容を表示する
+//
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    // CellにtableViewCellって名前を付ける事　忘れずに！
+    UITableViewCell *cell= [tableView dequeueReusableCellWithIdentifier:@"tableViewCell"];
+ 
+
+    UILabel *sectionLabel = (UILabel *)[cell viewWithTag:1];
+    UILabel *rowLabel = (UILabel *)[cell viewWithTag:2];
+
+    sectionLabel.text = [NSString stringWithFormat:@"section %ld", (long)indexPath.section];
+    rowLabel.text = [NSString stringWithFormat:@"row %ld", indexPath.row];
+
+
+    
+    return cell;
+}
+
+
+//
+// 2.行数を返す
+//
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 3;
+}
+
+//
+// 1.セクション数を返す
+//
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+
+
 
 @end
---
+++ readme.txt
@@ -0,0 +1,5 @@
+UITableView
+
+その3
+
+Cellをカスタムにする
